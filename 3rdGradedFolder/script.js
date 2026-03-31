0dde6bd2e082a2d9aa925ac974eb3d0f331c0b29
⋆˚࿔ 𝐚𝐬𝐭é𝐫𝐢 ! 𝜗𝜚˚⋆✮ <b2029lsaayes@pshs.edu.ph>
2026-03-31 19:08:40 +0800

    Fix style properties for point positioning
    Corrected property names for point positioning.

--- 3rdGradedFolder/script.js
+++ 3rdGradedFolder/script.js
@@ -1,25 +1,35 @@
 function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
+    // edit: variables must be grabbed from the input elements first
+    var x0 = parseInt(document.getElementById('N').value);
+    var y0 = parseInt(document.getElementById('M').value);
+    var x = parseInt(document.getElementById('X').value);
+    var y = parseInt(document.getElementById('Y').value);
+    
+    var in1 = document.getElementById('in1');
+    var out1 = document.getElementById('out1');
+
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
+
     var point = document.createElement('div');  
     point.className = 'point';
-    /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+
+    /* edit: Changed styleleft to style.left and stylebottom to style.bottom 
+       (Coordinate + Offset to center - half of point width)
+    */
+    point.style.left = (x - x0 + 200 - 5) + 'px'; 
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; 
+    
     document.getElementById('coordinatePlane').appendChild(point);
 
-    if (x > x0 && y > y0){
-      out1.innerHTML = "NE"
+    if (x === x0 || y === y0) {
+        out1.innerHTML = "divisa";
+    } else if (x > x0 && y > y0) {
+        out1.innerHTML = "NE";
+    } else if (x < x0 && y < y0) {
+        out1.innerHTML = "SO";
+    } else if (x > x0 && y < y0) {
+        out1.innerHTML = "SE";
+    } else if (x < x0 && y > y0) {
+        out1.innerHTML = "NO";
     }
-    else if (x < x0 && y < y0){
-      out1.innerHTML = "SO"
-    }
-    else if (x > x0 && y < y0){
-      out1.innerHTML = "SE"
-    }
-    else if (x < x0 && y > y0){
-      out1.innerHTML = "NO"
-    }
-    else{
-      out1.innerHTML = "divisa"
-    }
+}
